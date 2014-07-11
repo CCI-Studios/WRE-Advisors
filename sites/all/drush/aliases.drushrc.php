ebeb050c5a0cb1b10c6febfae6aec70a6a7b93c7
Chris Morris <cmorris@ccistudios.com>
2014-07-11 15:58:46 -0400

    Updated modules and Features

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -10,3 +10,13 @@ $aliases['dev'] = array(
 	)
 );
 
+$aliases['live'] = array(
+	'uri'=> 'live.wreadvisors.ca',
+	'root' => '/home/wreadvis/subdomains/live/public_html',
+	'remote-host'=> 'host.lambtonshield.com',
+	'remote-user'=> 'wreadvis',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
+
